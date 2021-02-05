4a83d9f9880821d1edc392a11b8caead45d1f1a2
Douglas Wille <willedouglas10@gmail.com>
2021-02-05 02:44:24 -0300

    feat: added router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import components from '@components';
+import router from 'src/router';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+components.forEach(component => app.component(component.name, component));
+
+app.use(router);
+
+app.mount('#app');
---
+++ src/router/routes.js
@@ -0,0 +1,11 @@
+import SavingGoalPage from '@modules/savingGoal/pages/SavingGoalPage.vue';
+
+const routes = [
+  {
+    path: '/saving-goal',
+    name: 'Saving Goal',
+    component: SavingGoalPage,
+  },
+];
+
+export default routes;
