f122831c866a3e0fc302679873fcf2d5b5ac448c
Douglas Wille <willedouglas10@gmail.com>
2021-02-05 02:46:22 -0300

    feat: added initial page display

---
+++ src/modules/savingGoal/components/SavingGoalForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <card>
+    <panel
+      title="Buy a house"
+      icon="house"
+      description="Saving goal"
+    />
+    <money-input />
+    <date-input />
+  </card>
+</template>
+
+<script>
+export default {
+  name: 'SavingGoalForm',
+}
+</script>
---
+++ src/modules/savingGoal/pages/SavingGoalPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="saving-goal-page">
+    <div class="saving-goal-page__subtitle">
+      <heading
+        type="primary"
+        size="lg"
+      >
+        Let's plan your <b>saving goal.</b>
+      </heading>
+    </div>
+    <saving-goal-form />
+  </div>
+</template>
+
+<script>
+import SavingGoalForm from '@modules/savingGoal/components/SavingGoalForm.vue';
+
+export default {
+  name: 'SavingGoalPage',
+  components: {
+    SavingGoalForm,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.saving-goal-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .saving-goal-page__subtitle {
+    margin-top: 48px;
+    margin-bottom: 24px;
+  }
+}
+</style>
